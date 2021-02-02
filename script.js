ec40b564f1884e16abb1f9b992824890e99f5cce
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-02-02 21:42:25 +0600

    testing completed

--- script.js
+++ script.js
@@ -3,46 +3,64 @@ var currentPIN;
 var randomGenerator = function(){
     var random = Math.floor(Math.random() * (9999- 1001) + 1000);
     currentPIN = random;
-    showinBox("gen-PIN", random);
+    showinBox(generateBox, random);
 }
-function showinBox(id, value){
-    var inputBox = document.getElementById(id);
+function showinBox(Box, value){
+    if(incorrectInfo.style.display != "none"){
+        incorrectInfo.style.display = "none";
+    }
     // console.log(value);
-    inputBox.value = value;
+    Box.value = value;
 }
 var numClicked = function(){
     if(inputNums.length == 4){
         return;
     }
     inputNums += this.innerText;
-    showinBox("input-num", inputNums);
+    showinBox(inputBox, inputNums);
 }
 var deleteOneCha = function(){
     inputNums = inputNums.slice(0, -1);
     // console.log(inputNums);
-    showinBox("input-num", inputNums);
+    showinBox(inputBox, inputNums);
 }
 var deleteAllNums = function(){
     inputNums = "";
-    showinBox("input-num", inputNums);
+    showinBox(inputBox, inputNums);
 }
 var submitted = function(){
     if(currentPIN == inputNums){
-        console.log("It works");
+        sectionRandom.style.display = "none";
+        sectionInput.style.display = "none";
+        correctInfo.style.display = "block";
     }
     else{
-        console.log("hello no")
+        incorrectInfo.style.display = "block";
+        inputNums = "";
+        inputBox.value = "";
     }
+}
+var playAgain = function(){
+    sectionRandom.style.display = "inherit";
+    location.reload();
 }
 var generatePIN = document.getElementsByClassName("btn-pin")[0];
 var nums= document.getElementsByClassName("num");
 var deleteOne = document.getElementById("del");
 var deleteAll = document.getElementById("clear");
 var submitBtn = document.getElementById("submit");
+var sectionRandom = document.getElementsByClassName("random-generator")[0];
+var sectionInput = document.getElementsByClassName("input-pin")[0];
+var correctInfo = document.getElementById("correct");
+var incorrectInfo = document.getElementById("incorrect");
+var generateBox = document.getElementById("gen-PIN");
+var inputBox = document.getElementById("input-num");
+var playAgainbtn = document.getElementById("play-again");
 generatePIN.addEventListener("click", randomGenerator);
 deleteOne.addEventListener("click", deleteOneCha);
 deleteAll.addEventListener("click", deleteAllNums);
 submitBtn.addEventListener("click", submitted);
+playAgainbtn.addEventListener("click", playAgain);
 for( var i = 0 ; i < nums.length ; i++){
     var singlenum = nums[i];
     singlenum.addEventListener("click", numClicked);
